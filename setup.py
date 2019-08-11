071894806a2dfb656bae055dcc9e0b966595de8f
pi <alexander.antaris@gmail.com>
2019-08-10 22:22:06 -0700

    standardizing python repo

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='canine_cannon',
+      version='0.0',
+      description='molesting dogs with bullets',
+      author='tigger')
